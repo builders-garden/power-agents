c6b38b1a6ca0175a78848391f45a63a08fc8c36d
PaoloRollo <paolo.rollo1997@gmail.com>
2024-10-18 22:07:37 -0700

    feat: add cron file.

---
+++ src/lib/cron.ts
@@ -0,0 +1,45 @@
+import { BrianCoinbaseSDK } from "@brian-ai/cdp-sdk";
+import { supabase } from "./supabase.js";
+
+export const handleRecurringTransactions = async () => {
+  const { data: recurrings } = await supabase.from("recurring").select("*");
+
+  if (!recurrings) return;
+
+  const toBeExecuted = [];
+
+  for (const recurring of recurrings) {
+    if (recurring.lastExection < new Date().getTime() + recurring.interval) {
+      toBeExecuted.push(recurring);
+    }
+  }
+
+  for (const transaction of toBeExecuted) {
+    const { data: agentData } = await supabase
+      .from("agents")
+      .select("*")
+      .eq("id", transaction.agentId)
+      .single();
+
+    if (agentData) {
+      const brianCDPSDK = new BrianCoinbaseSDK({
+        brianApiKey: process.env.BRIAN_API_KEY!,
+        coinbaseApiKeyName: process.env.CDP_SDK_API_KEY_NAME,
+        coinbaseApiKeySecret: process.env.CDP_SDK_API_KEY_SECRET,
+      });
+
+      brianCDPSDK.importWallet(agentData.mpcData);
+
+      await brianCDPSDK.transact(transaction.command);
+
+      await supabase
+        .from("recurring")
+        .update({
+          lastExection: new Date().getTime(),
+        })
+        .eq("id", transaction.id);
+    }
+  }
+};
+
+export const handleLimitTransactions = async () => {};
